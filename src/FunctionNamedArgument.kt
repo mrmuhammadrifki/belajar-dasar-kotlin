816e060799a28ef88d7d2e5eab46f5503f0880d0
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-09-01 18:20:00 +0700

    feat(function): create function default parameter and function named argument

---
+++ src/FunctionNamedArgument.kt
@@ -0,0 +1,15 @@
+fun fullName(
+    firstName: String,
+    middleName: String,
+    lastName: String
+) {
+    println("Hello $firstName $middleName $lastName")
+}
+
+fun main() {
+    fullName(
+        firstName = "Muhammad",
+        lastName = "Saputra",
+        middleName = "Rifki"
+    )
+}
